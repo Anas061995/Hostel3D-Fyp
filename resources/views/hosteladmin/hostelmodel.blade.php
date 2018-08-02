116bf120c3fefbc956f2c9afd2737cbafd4ef0c9
aniqakhan <aniqk755@gmail.com>
2018-08-02 22:38:24 +0500

    Revert "Merge branch 'master' of https://github.com/Anas061995/Hostel3D-Fyp"
    This reverts commit bf2803d7c9191cdd16f6d300eb5a73ef20d05ff6, reversing
    changes made to 6ede140cea378104da6721dbc854c40c83a18d05.

--- resources/views/hosteladmin/hostelmodel.blade.php
+++ resources/views/hosteladmin/hostelmodel.blade.php
@@ -9,7 +9,6 @@
   <link rel="stylesheet" href="{{asset('asset/css/AdminLTE.min.css')}}">
   <link rel="stylesheet" href="{{asset('asset/css/skins/skin-blue.min.css')}}">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 @endsection
 @section('content')
 @include('includes.nav')
@@ -28,80 +27,31 @@
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
         <li class="active">Here</li>
       </ol>
+
+
+
+
     </section>
-<section>
+
     <!-- Main content -->
-    <div class="container">
-  <div class="col-md-10">
-      <div class="form-group">
-          <label>Upload 3D Model</label>
-          <div class="input-group">
-              <span class="input-group-btn">
-                  <span class="btn btn-default btn-file">
-                      Browse… <input type="file" id="imgInp">
-                  </span>
-              </span>
-              <input type="text" class="form-control" readonly>
-          </div>
-          <br>
-      </div>
-      <div>
-        <button type="button" class="btn btn-success">Upload</button>
-      </div>
-      <br></br>
-      <center><image id='img-upload'/></centr>
-    </div>
+    <section class="content container-fluid">
+
+
+
+    </section>
+
   </div>
-</section>
-</div>
-@include('includes.footer')
+
+    @include('includes.footer')
+
 </div>
 @endsection
 
 @section('scripts')
-<script>
-$(document).ready( function() {
-    	$(document).on('change', '.btn-file :file', function() {
-		var input = $(this),
-			label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-		input.trigger('fileselect', [label]);
-		});
-
-		$('.btn-file :file').on('fileselect', function(event, label) {
-
-		    var input = $(this).parents('.input-group').find(':text'),
-		        log = label;
-
-		    if( input.length ) {
-		        input.val(log);
-		    } else {
-		        if( log ) alert(log);
-		    }
-
-		});
-		function readURL(input) {
-		    if (input.files && input.files[0]) {
-		        var reader = new FileReader();
-
-		        reader.onload = function (e) {
-		            $('#img-upload').attr('src', e.target.result);
-		        }
-
-		        reader.readAsDataURL(input.files[0]);
-		    }
-		}
-
-		$("#imgInp").change(function(){
-		    readURL(this);
-		});
-	});
-  </script>
 <script src="{{asset('asset/bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="{{asset('asset/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset('asset/js/adminlte.min.js')}}"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQ384eNh2btXKG7M__gzqPAOKoq6skHOY&callback=myMap"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 @endsection
